53e167572888db7e2b64a9988562c0e6a80cb7af
Savpek <savolainen.pekka@gmail.com>
2017-03-09 14:48:01 +0200

    Small structural tweaks.

---
+++ src/driver/acr1222l.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Driver = (function () {
+    function Driver() {
+    }
+    return Driver;
+}());
+exports.Driver = Driver;
+//# sourceMappingURL=acr1222l.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import { Driver } from './driver/acr1222l';
+
 export class NfcReader {
 }
