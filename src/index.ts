a60c7de7f9539001b4110515957206efc7af4e7d
Savpek <savolainen.pekka@gmail.com>
2017-04-03 11:40:07 +0300

    Fixed type exports.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { IDriver } from './driver/IDriver';
-import { DriverMessage } from './driver/DriverMessage';
-import { ILogger } from './driver/ILogger';
+export { IDriver } from './driver/IDriver';
+export { DriverMessage } from './driver/DriverMessage';
+export { ILogger } from './driver/ILogger';
 export { Acr1222l } from './driver/acr1222l';
