bb7356975033391beb55c7f1f807c8c8d4e0ba70
Savpek <savolainen.pekka@gmail.com>
2017-04-03 11:33:34 +0300

    Removed compiled files.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export class NfcReader {
-}
+import { IDriver } from './driver/IDriver';
+import { DriverMessage } from './driver/DriverMessage';
+import { ILogger } from './driver/ILogger';
+export { Acr1222l } from './driver/acr1222l';
